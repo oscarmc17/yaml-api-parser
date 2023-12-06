c4c4611c58849b0937ab50b2b3451350edc55641
Oscar Caicedo <oscarccd17@gmail.com>
2023-12-05 21:37:19 -0600

    parsing function created w/ data return

--- health_checker2.py
+++ health_checker2.py
@@ -1,8 +1,20 @@
-import yaml, json
+import yaml, json, requests
 
 url = 'config.yaml'
 
 def parse(file_path):
     with open(file_path, "r") as f:
-        print(f)
+        config = yaml.safe_load(f)
+        
+        return config
 
+
+def healthCheck(config):
+    
+    for data in config:
+        print(json.dumps(data, indent=4))
+
+
+
+parser = parse(url)
+healthCheck(parser)
